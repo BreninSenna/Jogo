b6c141130c0c7dd9fb996ba6c0c785df377beb24
BreninSenna <127742561+BreninSenna@users.noreply.github.com>
2023-10-02 08:50:54 -0300

    Add files via upload

---
+++ Jogo/jogo/js/main.js
@@ -0,0 +1,14 @@
+// Criar variaveis no Javascript
+//var letra = "o"; var é uma versão em desuso
+//const letra = 'd';
+//console.log(letra);
+
+//Iniciando uma variável que representa um jogador
+let letra = 'X';
+function joga(cel) {
+    let casaClicada = document.getElementById(cel).innerHTML;
+    //innerHTML injeta tags HTML na página
+    if(casaClicada == 'X') || casaClicada="O") {
+        
+    }
+}
